afa7bf7743da6290f1d52d772d0e9043105b2a1f
Gustavo Paco <gustavopaco@gmail.com>
2023-09-19 13:16:22 -0300

    ✨ Adiciona classe UserService e UserController
    Adiciona classe UserService com métodos para listar habilidades de um usuário, criar habilidade, atualizar habilidade e excluir habilidade.
    
    Adiciona classe UserController com métodos para listar habilidades de um usuário, criar habilidade, atualizar habilidade e excluir habilidade.

---
+++ src/main/java/com/pacoprojects/portfolio/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.pacoprojects.portfolio.controller;
+
+import com.pacoprojects.portfolio.dto.SkillDto;
+import com.pacoprojects.portfolio.dto.SkillProjection;
+import com.pacoprojects.portfolio.service.UserService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@Validated
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<List<SkillProjection>> listSkillsByUser(@PathVariable @NotNull Long id) {
+        return ResponseEntity.ok(userService.listSkillsByUser(id));
+    }
+
+    @PostMapping("skill")
+    public ResponseEntity<Void> createSkill(@RequestBody @Valid @NotNull SkillDto skillDto) {
+        final String LOCATION = "/user/skill/";
+        return ResponseEntity.created(URI.create(LOCATION + userService.createSkill(skillDto).id())).build();
+    }
+
+    @PutMapping("skill")
+    public ResponseEntity<Void> updateSkill(@RequestBody @Valid @NotNull SkillDto skillDto) {
+        userService.updateSkill(skillDto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("skill/{id}")
+    public ResponseEntity<Void> deleteSkill(@PathVariable @NotNull Long id) {
+        userService.deleteSkill(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/pacoprojects/portfolio/service/UserService.java
@@ -0,0 +1,53 @@
+package com.pacoprojects.portfolio.service;
+
+import com.pacoprojects.portfolio.constants.Messages;
+import com.pacoprojects.portfolio.dto.SkillDto;
+import com.pacoprojects.portfolio.dto.SkillProjection;
+import com.pacoprojects.portfolio.exception.RecordNotFoundException;
+import com.pacoprojects.portfolio.mapper.SkillMapper;
+import com.pacoprojects.portfolio.repository.SkillRepository;
+import com.pacoprojects.portfolio.repository.UserApplicationRepository;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserApplicationRepository userApplicationRepository;
+    private final SkillRepository skillRepository;
+    private final SkillMapper skillMapper;
+
+    public List<SkillProjection> listSkillsByUser(@NotNull Long id) {
+        return userApplicationRepository.findById(id)
+                .map(user -> skillRepository.findAllByUserApplicationId(user.getId()))
+                .orElseThrow(() -> new RecordNotFoundException(Messages.USER_NOT_FOUND));
+    }
+
+    public SkillDto createSkill(@NotNull SkillDto skillDto) {
+        validateUser(skillDto.userApplication().id());
+        return skillMapper.toDto(skillRepository.save(skillMapper.toEntity(skillDto)));
+    }
+
+    private void validateUser(@NotNull Long id) {
+        userApplicationRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(Messages.USER_NOT_FOUND));
+    }
+
+    public void updateSkill(@NotNull SkillDto skillDto) {
+        skillRepository.findById(skillDto.id())
+                .ifPresentOrElse(skill -> skillRepository.save(skillMapper.partialUpdate(skillDto, skill)),
+                        () -> {throw new RecordNotFoundException(Messages.SKILL_NOT_FOUND);}
+                );
+    }
+
+    public void deleteSkill(@NotNull Long id) {
+        skillRepository.findById(id)
+                .ifPresentOrElse(skillRepository::delete, () -> {
+                    throw new RecordNotFoundException(Messages.SKILL_NOT_FOUND);
+                });
+    }
+}
